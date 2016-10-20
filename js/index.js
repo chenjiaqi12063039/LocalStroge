3629ccc9c64900b62816b6aa381906d367f24cd0
chenjiaqi12063039 <278831372@qq.com>
2016-10-20 15:54:04 +0100

    update6

--- js/index.js
+++ js/index.js
@@ -2,17 +2,7 @@ var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
 
-var nutritional={"Chocolate":[
-    {"ProductName":"SNICKERS & Hazelnut 4 Bar Multipack 196g",
-    "Energy":"2047kj/100g",
-    "Energy":"489kcal/100g",
-    "Fat":"23.9g/100g",
-    "Saturates":"9.0g/100g",
-    "Carbohydrate":"59.5g/100g",
-    "Sugars":"50.4g/100g",
-    "Protein":"8.1g/100g",
-    "Salt":"0.59/100g"},
-]}
+    
 
 
     function onLoad() {
@@ -21,6 +11,4 @@ var nutritional={"Chocolate":[
     }
 
     function onDeviceReady() {
-        var output = "Chocolate";
-        alert(output);
     }
