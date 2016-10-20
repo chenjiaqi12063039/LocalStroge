314229a07dd0f73ae836a71ca6b2961a21fd87f8
chenjiaqi12063039 <278831372@qq.com>
2016-10-20 14:16:27 +0100

    Update

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var paused_count =0;
+var resumed_count = 0;
+var launched_count = 0;
+
+    function onLoad() {
+        document.addEventListener("deviceready", onDeviceReady, false);
+		console.log("device ready");
+    }
+
+    function onDeviceReady() {
+		alert("device ready");
+
+    }
